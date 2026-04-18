fc09261987fe3b14ece2cc62b468dd98accca600
laricoDGT <larico@dgtalliance.com>
2026-04-18 00:12:10 -0500

    fix: serve index.html at root path

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/extract", (req, res) => {
   const { url } = req.query;
 
